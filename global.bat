1e48dde77fe48e1af50e34ecef0bad84904d999c
vadosnaprimer <feos-theos@yandex.ru>
2017-11-18 16:36:37 +0300

    another awesome feature by Dacicus, sending multiple HQ scales at once didn't test!

--- global.bat
+++ global.bat
@@ -8,6 +8,8 @@
 @echo off
 setlocal enableextensions
 
+if [%1]==[/?] goto syntax_cmd
+
 :: Restore AVS defaults ::
 ".\programs\replacetext" "encode.avs" "pass = 1" "pass = 0"
 ".\programs\replacetext" "encode.avs" "pass = 2" "pass = 0"
@@ -25,9 +27,7 @@ echo -----------------------
 echo  Hybrid Encoding Batch
 echo -----------------------
 echo.
-echo Command-line arguments: global.bat ^<arc^> ^<enc_opt^>
-echo   ^<arc^>       Aspect ratio?    1-3 or width:height
-echo   ^<enc_opt^>   Encode option?   1-5
+echo Type /? for command-line arguments.
 echo.
 
 if [%1]==[] goto SAR_OPTIONS
@@ -84,7 +84,7 @@ if [%ar_w%]==[] goto Get_AR
 if [%ar_h%]==[] goto Get_AR
 if %ar_w% leq 0 goto Get_AR
 if %ar_h% leq 0 goto Get_AR
-goto TV Sar
+goto TV_Sar
 
 : TV_SAR
 for /f "tokens=2 skip=2 delims== " %%G in ('find "wAspect = " "%~dp0encode.avs"') do (set current_wAspect=%%G)
@@ -111,12 +111,13 @@ set VAR=1:1
 goto ENCODE_OPTIONS
 
 : ENCODE_OPTIONS
-if [%2]==[1] goto 10bit444
-if [%2]==[2] goto 512kb
-if [%2]==[3] goto HD
-if [%2]==[4] goto HD
-if [%2]==[5] goto ExtraHQ
+set EncodeChoice=
+set param_2=%2
+if [%param_2%]==[] goto get_encode_option
+set EncodeChoice=%param_2:~0,1%
+goto process_encode_option
 
+:get_encode_option
 echo.
 echo What encode do you want to do?
 echo.
@@ -124,11 +125,11 @@ echo Press 1 for Modern HQ MKV.
 echo Press 2 for Compatibility MP4.
 echo Press 3 for HD Stream.
 echo Press 4 for All of the above.
-echo Press 5 for extra HQ encodes.
+echo Press 5 for Extra HQ encodes.
 echo.
-
-: Set_choice
 set /p EncodeChoice=
+
+:process_encode_option
 if "%EncodeChoice%"=="1" goto 10bit444
 if "%EncodeChoice%"=="2" goto 512kb
 if "%EncodeChoice%"=="3" goto HD
@@ -137,46 +138,71 @@ if "%EncodeChoice%"=="5" goto ExtraHQ_scale
 
 echo.
 echo You better choose something real!
-goto Set_choice
+goto get_encode_option
 
 : ExtraHQ_scale
 ".\programs\replacetext" "encode.avs" "hq = false" "hq = true"
-echo.
-echo What scale factor do you want to use? (2/3/4)
-echo.
 
-set /p ExtraScale=
-if "%ExtraScale%" GTR "1" (if "%ExtraScale%" LSS "5" goto ExtraHQ_type)
+set ExtraScaleFactors=
+if [%3]==[] goto get_extrahq_scale
+set ExtraScaleFactors=%3
+goto remove_starting_slash
+
+:get_extrahq_scale
+echo.
+set /p ExtraScaleFactors=What scale factor do you want to use? (2-16, separate multiple with /)
+
+:remove_starting_slash
+if [%ExtraScaleFactors%]==[] goto get_extrahq_scale
+if [%ExtraScaleFactors:~0,1%]==[/] (
+ set ExtraScaleFactors=%ExtraScaleFactors:~1%
+ goto remove_starting_slash
+)
+
+set ExtraScale=
+for /f "tokens=1 delims=/" %%G in ('echo %ExtraScaleFactors%') do (set ExtraScale=%%G)
+if [%ExtraScale%]==[] goto get_extrahq_scale
+
+:verify_extrahq_scale
+if %ExtraScale% GTR 1 (if %ExtraScale% LSS 17 goto ExtraHQ_type)
 echo.
 echo Uhhh... Seriously?
-goto ExtraHQ_scale
+goto get_extrahq_scale
 
 : ExtraHQ_type
 for /f "tokens=3 skip=2" %%G in ('find "hq_scale = " "%~dp0encode.avs"') do (set current_scale=%%G)
 ".\programs\replacetext" "encode.avs" "hq_scale = %current_scale%" "hq_scale = %ExtraScale%"
 
+set ExtraType=
+if [%param_2%]==[] goto get_extrahq_type
+if [%param_2:~1,1%]==[] goto get_extrahq_type
+set ExtraType=%param_2:~1,1%
+goto process_extrahq_type
+
+:get_extrahq_type
 echo.
-echo Which ExtraHQ encode type do you want to do?
+echo Which ExtraHQ encode type do you want to do using scale %ExtraScale%?
 echo.
 echo Press 1 for Modern HQ MKV.
 echo Press 2 for Compatibility MP4.
 echo Press 3 both.
 echo.
-
 set /p ExtraType=
+
+:process_extrahq_type
 if "%ExtraType%"=="1" goto ExtraHQ_10bit444
 if "%ExtraType%"=="2" goto ExtraHQ_512kb
 if "%ExtraType%"=="3" goto ExtraHQ_10bit444
 echo.
 echo C'mon! Let's finish this already!
-goto ExtraHQ_type
+goto get_extrahq_type
 
 : HD
 :: Audio ::
 ".\programs\avs2pipemod" -wav encode.avs | ".\programs\venc" -q10 - ".\temp\audio_youtube.ogg"
 echo.
 echo ----------------------------
-echo  Encoding YouTube HD stream 
+echo  Encoding YouTube HD stream
 echo ----------------------------
 echo.
 :: Video ::
@@ -191,20 +217,19 @@ if "%VIRTUALBOY%"=="1" (".\programs\ffmpeg" -i ".\output\encode_youtube.mkv" -c 
 
 echo.
 echo -----------------------------
-echo  Uploading YouTube HD stream 
+echo  Uploading YouTube HD stream
 echo -----------------------------
 echo.
 start "" cmd /c type "%~dp0programs\ytdesc.txt" ^| "%~dp0programs\tvcman.exe" "%~dp0output\encode%VBPREF%youtube.mkv" todo tasvideos
 rem start "" cmd /c echo todo ^| "%~dp0programs\tvcman.exe" "%~dp0output\encode%VBPREF%youtube.mkv" todo tasvideos
 if "%EncodeChoice%"=="3" goto Defaults
-if [%2]==[3] goto Defaults
 
 : 10bit444
 :: Audio ::
 ".\programs\avs2pipemod" -wav encode.avs | ".\programs\sox" -t wav - -t wav - trim 0.0065 | ".\programs\opusenc" --bitrate 64 --padding 0 - ".\temp\audio.opus"
 echo.
 echo ----------------------
-echo  Generating timecodes 
+echo  Generating timecodes
 echo ----------------------
 :: Timecodes ::
 ".\programs\replacetext" "encode.avs" "pass = 0" "pass = 1"
@@ -212,7 +237,7 @@ echo ----------------------
 ".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
 echo.
 echo --------------------------------
-echo  Encoding 10bit444 downloadable 
+echo  Encoding 10bit444 downloadable
 echo --------------------------------
 echo.
 :: Video ::
@@ -221,14 +246,13 @@ echo.
 :: Muxing ::
 ".\programs\mkvmerge" -o ".\output\encode.mkv" --timecodes -1:".\temp\times.txt" ".\temp\video.mkv" ".\temp\audio.opus"
  if "%EncodeChoice%"=="1" goto Defaults
- if [%2]==[1] goto Defaults
 
 : 512kb
 :: Audio ::
 ".\programs\avs2pipemod" -wav encode.avs | ".\programs\qaac64" -v 0 --he -q 2 --delay -5187s --threading --no-smart-padding - -o ".\temp\audio.mp4"
 echo.
 echo -------------------------------
-echo  Encoding Archive 512kb stream 
+echo  Encoding Archive 512kb stream
 echo -------------------------------
 echo.
 :: Video ::
@@ -247,7 +271,7 @@ goto Defaults
 ".\programs\avs2pipemod" -wav encode.avs | ".\programs\sox" -t wav - -t wav - trim 0.0065 | ".\programs\opusenc" --bitrate 64 --padding 0 - ".\temp\audio_extra.opus"
 echo.
 echo ----------------------
-echo  Generating timecodes 
+echo  Generating timecodes
 echo ----------------------
 echo.
 :: Timecodes ::
@@ -256,7 +280,7 @@ echo.
 ".\programs\replacetext" "encode.avs" "pass = 1" "pass = 2"
 echo.
 echo --------------------------------
-echo  Encoding ExtraHQ downloadable 
+echo  Encoding ExtraHQ downloadable
 echo --------------------------------
 echo.
 :: Video ::
@@ -264,7 +288,7 @@ echo.
 ".\programs\avs2pipemod" -y4mp encode.avs | ".\programs\x264-10_x64" --threads auto --sar "%VAR%" --crf 20 --keyint 600 --preset veryslow --input-range pc --range pc --tcfile-in ".\temp\times.txt" -o ".\temp\video_%ExtraScale%x.mkv" --colormatrix smpte170m --output-csp i444  --demuxer y4m -
 :: Muxing ::
 ".\programs\mkvmerge" -o ".\output\encode_%ExtraScale%x.mkv" --timecodes -1:".\temp\times.txt" ".\temp\video_%ExtraScale%x.mkv" ".\temp\audio_extra.opus"
-if "%ExtraType%" NEQ "3" goto Defaults
+if "%ExtraType%" NEQ "3" goto check_more_hqfactors
 
 : ExtraHQ_512kb
 :: Extra 512kb ::
@@ -272,7 +296,7 @@ if "%ExtraType%" NEQ "3" goto Defaults
 ".\programs\avs2pipemod" -wav encode.avs | ".\programs\qaac64" -v 0 --he -q 2 --delay -5187s --threading --no-smart-padding - -o ".\temp\audio_extra.mp4"
 echo.
 echo -------------------------------
-echo  Encoding ExtraHQ stream 
+echo  Encoding ExtraHQ stream
 echo -------------------------------
 echo.
 :: Video ::
@@ -283,7 +307,29 @@ echo.
 for /f "tokens=2" %%i in ('%~dp0programs\avs2pipemod -info %~dp0encode.avs ^|find "fps"') do (set fps=%%i)
 for /f %%k in ('%~dp0programs\div %fps%') do (set double=%%k)
 ".\programs\mp4box_x64" -hint -add ".\temp\video_%ExtraScale%x_512kb.h264":fps=%double% -add ".\temp\audio_extra.mp4" -new ".\output\encode_%ExtraScale%x_512kb.mp4"
-goto Defaults
+goto check_more_hqfactors
+
+:check_more_hqfactors
+set _scalestringlength=0
+
+:calculate_length
+call set _dummy=%%ExtraScale:~%_scalestringlength%%%
+if not defined _dummy (
+  goto updateExtraScaleFactors
+)
+set /a _scalestringlength=%_scalestringlength%+1
+goto calculate_length
+
+:updateExtraScaleFactors
+call set ExtraScaleFactors=%%ExtraScaleFactors:~%_scalestringlength%%%
+if [%ExtraScaleFactors%]==[] goto Defaults
+if [%ExtraScaleFactors:/=%]==[] goto Defaults
+
+set ExtraScaleFactors=%ExtraScaleFactors:~1%
+
+if [%ExtraScaleFactors%]==[] goto Defaults
+if [%ExtraScaleFactors:/=%]==[] goto Defaults
+goto remove_starting_slash
 
 : Defaults
 ".\programs\replacetext" "encode.avs" "pass = 1" "pass = 0"
@@ -292,3 +338,30 @@ goto Defaults
 ".\programs\replacetext" "encode.avs" "hd = true" "hd = false"
 ".\programs\replacetext" "encode.avs" "hq = true" "hq = false"
 ".\programs\replacetext" "encode.avs" "vb = true" "vb = false"
+goto end
+
+:syntax_cmd
+echo.
+echo Command-line arguments: %0 ^<arc^> ^<enc_opt^>^<hq_opt^> ^<hq_scale^>
+echo   ^<arc^>       Aspect ratio?      1-3 or width:height
+echo                 1 = 1:1
+echo                 2 = 4:3
+echo                 3 = 16:9
+echo   ^<enc_opt^>   Encode option?     1-5
+echo                 1 = Modern HQ MKV
+echo                 2 = Compatibility MP4
+echo                 3 = HD Stream
+echo                 4 = All of the above
+echo                 5 = Extra HQ encodes
+echo   ^<hq_opt^>    Extra HQ option?   1-3
+echo                 1 = Modern HQ MKV
+echo                 2 = Compatibility MP4
+echo                 3 = Both of the above
+echo   ^<hq_scale^>  Extra HQ scale?    2-16, separate multiple with /
+echo.
+echo   NOTE: hq_opt and hq_scale are used only when enc_opt is 5.
+echo   NOTE: Do not put spaces between enc_opt and hq_opt.  For example, enter 52 to
+echo         indicate encode option 5 and extra HQ option 2.
+
+:end
+endlocal
